8e755597fa42c62da46c67f150ce6b69c19b391b
Norbert Melzer <timmelzer@gmail.com>
2017-04-29 14:12:55 +0200

    improve install-script

--- install.zsh
+++ install.zsh
@@ -1,17 +1,58 @@
 #!/usr/bin/env zsh
 
+### This script installs all the dotfiles, folders and scripts into the
+### appropriate locations.
+###
+### This script works by copying the files out of intention. It is meant to be
+### run on platforms that don't support symlinking. Perhaps there will be added
+### an option later on, that recognises those platforms. 
+
 autoload -U colors; colors
+
+if [[ $1 == "help" ]]; then
+    cat $0 | grep -E "^### ?" | sed -r 's|### ?||'
+    exit 0
+elif [[ $# -ne 0 ]]; then
+    cat $0 | grep -E "^### ?" | sed -r 's|### ?||'
+    exit 1
+fi
 
 local h1="${fg_bold[blue]}"
 local h2="${fg_bold[cyan]}"
 
 local default="${reset_color}"
 
-local ok=" [${fg[green]} OK ${default}]"
-local fail=" [${fg[red]}FAIL${default}]"
+local ok="\t[${fg[green]} OK ${default}]"
+local fail="\t[${fg[red]}FAIL${default}]"
+
+local cp="\t[${fg[green]} CP ${default}]"
+local skip="\t[${fg[yellow]}SKIP${default}]"
+
+function decorate () {
+    local opts=()
+    if [[ $1 == "-n" ]]; then
+        opts=($opts -n); shift
+    fi
+    local prefix=$1; shift
+    local suffix=$1; shift
+    echo $opts "$prefix$@$suffix" 
+}
+
+function h1 () {
+    decorate $h1 $default "*** $@ ***"
+}
+
+function h2 () {
+    if [[ $1 == "-n" ]]; then
+        shift
+        decorate -n $h2 $default " *  $@"
+    else
+        decorate $h2 $default " *  $@"
+    fi
+}
 
 function check () {
-    echo -n "$h2 *  Check if '$1' is available\t${default}"
+    h2 -n Check if "'$1'" is available
     if which $1 > /dev/null 2> /dev/null; then
         echo $ok
     else
@@ -28,28 +69,34 @@ function compare_and_copy_files () {
     local sum_dest=`sha1sum $dest | cut -d ' ' -f 1`
 
     if [[ $sum_src != $sum_dest ]]; then
+        echo $cp
         cp $src $dest
+    else
+        echo $skip
     fi
+}
+
+function cp_files () {
+    for src in $@; do
+        local abs_src=`readlink -f $src`
+        local dest=$HOME/${src#home/}
+        h2 -n Copying "'$abs_src'" to "'$dest'"
+        compare_and_copy_files $abs_src $dest
+    done
 }
 
 LANG=C
 
-echo "$h1*** Prechecks ***$default"
+h1 Prechecks
 check sha1sum
 
-echo "$h1*** Finding files ***$default"
+h1 Finding files
 local files=(`find home -type f`)
 
-echo "$h1*** Copying files ***$default"
-for src in $files; do
-    local abs_src=`readlink -f $src`
-    local dest=$HOME/${src#home/}
-    echo "$h2 *  Copying $abs_src"
-    echo "$h2    -> $dest"
-    compare_and_copy_files $abs_src $dest
-done
+h1 "Copying files"
+cp_files $files
 
-echo "$h1*** Compiling files ***$default"
+h1 Compiling files
 pushd ~
 make -C .zsh
 popd
